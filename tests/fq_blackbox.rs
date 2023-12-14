cea47ec3eff852c2e4016237eb7ece9b777ad050
Michael Lodder <mike@litprotocol.com>
2023-12-14 09:06:38 -0700

    update to use latest library

--- tests/fq_blackbox.rs
+++ tests/fq_blackbox.rs
@@ -8,7 +8,7 @@ fn test_to_and_from_bytes() {
     let mut rng = new_rng();
     for _ in 0..NUM_BLACK_BOX_CHECKS {
         let a = Fq::new_random(&mut rng);
-        assert_eq!(a, Fq::from_bytes(&Fq::to_bytes(&a)).unwrap());
+        assert_eq!(a, Fq::from_le_bytes(&Fq::to_le_bytes(&a)).unwrap());
     }
 }
 
@@ -28,8 +28,8 @@ fn test_additive_identity() {
     let mut rng = new_rng();
     for _ in 0..NUM_BLACK_BOX_CHECKS {
         let a = Fq::new_random(&mut rng);
-        assert_eq!(a, a + Fq::zero());
-        assert_eq!(a, Fq::zero() + a);
+        assert_eq!(a, a + Fq::ZERO);
+        assert_eq!(a, Fq::ZERO + a);
     }
 }
 
@@ -38,8 +38,8 @@ fn test_subtract_additive_identity() {
     let mut rng = new_rng();
     for _ in 0..NUM_BLACK_BOX_CHECKS {
         let a = Fq::new_random(&mut rng);
-        assert_eq!(a, a - Fq::zero());
-        assert_eq!(a, Fq::zero() - -&a);
+        assert_eq!(a, a - Fq::ZERO);
+        assert_eq!(a, Fq::ZERO - -&a);
     }
 }
 
@@ -49,8 +49,8 @@ fn test_additive_inverse() {
     for _ in 0..NUM_BLACK_BOX_CHECKS {
         let a = Fq::new_random(&mut rng);
         let a_neg = -&a;
-        assert_eq!(Fq::zero(), a + a_neg);
-        assert_eq!(Fq::zero(), a_neg + a);
+        assert_eq!(Fq::ZERO, a + a_neg);
+        assert_eq!(Fq::ZERO, a_neg + a);
     }
 }
 
@@ -81,8 +81,8 @@ fn test_multiplicative_identity() {
     let mut rng = new_rng();
     for _ in 0..NUM_BLACK_BOX_CHECKS {
         let a = Fq::new_random(&mut rng);
-        assert_eq!(a, a * Fq::one());
-        assert_eq!(a, Fq::one() * a);
+        assert_eq!(a, a * Fq::ONE);
+        assert_eq!(a, Fq::ONE * a);
     }
 }
 
@@ -91,12 +91,12 @@ fn test_multiplicative_inverse() {
     let mut rng = new_rng();
     for _ in 0..NUM_BLACK_BOX_CHECKS {
         let a = Fq::new_random(&mut rng);
-        if a == Fq::zero() {
+        if a == Fq::ZERO {
             continue;
         }
         let a_inv = a.invert().unwrap();
-        assert_eq!(Fq::one(), a * a_inv);
-        assert_eq!(Fq::one(), a_inv * a);
+        assert_eq!(Fq::ONE, a * a_inv);
+        assert_eq!(Fq::ONE, a_inv * a);
     }
 }
 
@@ -115,7 +115,7 @@ fn test_multiply_additive_identity() {
     let mut rng = new_rng();
     for _ in 0..NUM_BLACK_BOX_CHECKS {
         let a = Fq::new_random(&mut rng);
-        assert_eq!(Fq::zero(), Fq::zero() * a);
-        assert_eq!(Fq::zero(), a * Fq::zero());
+        assert_eq!(Fq::ZERO, Fq::ZERO * a);
+        assert_eq!(Fq::ZERO, a * Fq::ZERO);
     }
 }
